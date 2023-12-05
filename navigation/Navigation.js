1570320d6a1f680416d352d9cc378b5a03d26ab9
Salla Komppa <salla.halminen@hotmail.com>
2023-12-05 20:02:27 +0200

    Small updates

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -9,6 +9,8 @@ import Home from '../screens/Home';
 import Patterns from '../screens/Patterns';
 import Search from '../screens/Search';
 import Yarns from '../screens/Yarns';
+import ShoppingList from '../screens/ShoppingList';
+import Favorites from '../screens/Favorites';
 
 
 //    const Stack = createNativeStackNavigator();
@@ -37,6 +39,11 @@ export default function App() {
                     <Tab.Screen name="Search" icon="search" component={Search} />
                     <Tab.Screen name="Patterns" component={Patterns} />
                     <Tab.Screen name="Yarns" component={Yarns} />
+                    <Tab.Screen name="Shopping list" component={ShoppingList} />
+                    <Tab.Screen name="Favorites" component={Favorites} />
+
+                    
+
                 </Tab.Navigator>
             </NavigationContainer>
 
