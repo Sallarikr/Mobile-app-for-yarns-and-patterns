b6a56175cc0690d74a21e8c168ab91a4bd451238
Salla Komppa <salla.halminen@hotmail.com>
2023-12-10 19:44:02 +0200

    Update to navigation

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -1,54 +1,55 @@
+import * as React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import * as React from "react";
 import { Header } from '@rneui/themed';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-//import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import Home from '../screens/Home';
-import Patterns from '../screens/Patterns';
-import Search from '../screens/Search';
-import Yarns from '../screens/Yarns';
-import ShoppingList from '../screens/ShoppingList';
 import Favorites from '../screens/Favorites';
+import Search from '../screens/Search';
+import ShoppingList from '../screens/ShoppingList';
 
-
-//    const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-
-export default function App() {
+export default function Navigation() {
     return (
-
         <View style={styles.container}>
             <Header
-                backgroundColor="#FFCEEA"
+                backgroundColor="#d9a5cc"
                 centerComponent={{
                     text: "Yarn and pattern stash",
                     style: { color: "#fff", fontSize: 22 },
-
                 }}
                 leftComponent={{ icon: "menu", color: "#fff", size: 28 }}
                 rightComponent={{ icon: "person", color: "#fff", size: 28 }}
             />
-
-
             <NavigationContainer>
-                <Tab.Navigator>
-                <Tab.Screen name="Search" icon="search" component={Search} />
-
-
-                    <Tab.Screen name="Home" component={Home} />
-                    <Tab.Screen name="Patterns" component={Patterns} />
-                    <Tab.Screen name="Yarns" component={Yarns} />
-                    <Tab.Screen name="Shopping list" component={ShoppingList} />
+                <Tab.Navigator
+                    screenOptions={({ route }) => ({
+                        "tabBarActiveTintColor": "#d9a5cc",
+                        "tabBarInactiveTintColor": "lightgray",
+                        tabBarIcon: ({ focused, color, size }) => {
+                            let iconName;
+                            if (route.name === 'Search') {
+                                iconName = focused ? 'search' : 'search';
+                            } else if (route.name === 'Favorites') {
+                                iconName = focused ? 'heart' : 'heart';
+                            } else if (route.name === 'Home') {
+                                iconName = focused ? 'home' : 'home';
+                            } else if (route.name === 'Shopping List') {
+                                iconName = focused ? 'shopping-cart' : 'shopping-cart';
+                            }
+                            return <Icon name={iconName} size={size} color={color} />;
+                        },
+                    })}
+                >
+                    <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
+                    <Tab.Screen name="Search" component={Search} />
                     <Tab.Screen name="Favorites" component={Favorites} />
-
-                    
-
+                    <Tab.Screen name="Shopping List" component={ShoppingList} />
                 </Tab.Navigator>
             </NavigationContainer>
-
             <StatusBar style="auto" />
         </View>
     );
@@ -56,11 +57,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
     container: {
-        marginTop: 25,
+        paddingTop: 20,
         flex: 1,
-        backgroundColor: '#fff',
-    },
-    scrollview: {
-        marginBottom: 20,
+        backgroundColor: '#d9a5cc',
     },
 });
